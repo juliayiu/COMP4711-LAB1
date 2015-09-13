a2189332e6171086e09c6be4f4d865199b852a3d
Julia <jyiu93@gmail.com>
2015-09-13 05:31:47 -0700

    Added Comments

--- Student.php
+++ Student.php
@@ -1,6 +1,17 @@
 <?php
 
+/*
+ * Description of Student
+ * 
+ * @author Julia Yiu
+ */
+
 class Student {
+    /*
+     * This function is the constructor.
+     * It creates the first name, last name and 
+     * the arrays of emails and grades.
+     */
 
     function __construct() {
         $this->surname = '';
@@ -9,14 +20,33 @@ class Student {
         $this->grades = array();
     }
 
+    /**
+     * This function is a mutator method used to set the email address 
+     * each student may have and added it into the emails array.
+     * 
+     * @param string $which
+     * @param string $address
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * This function is a mutator method used to set the grade of the student
+     * by adding it into the grades array.
+     * 
+     * @param double $grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /**
+     * This function is used to calculate the student's
+     * average grade and returning the average grade.
+     * 
+     * @return double $total
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -24,6 +54,11 @@ class Student {
         return $total / count($this->grades);
     }
 
+    /**
+     * This function returns each student to string for reporting.
+     * 
+     * @return string
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
@@ -32,5 +67,4 @@ class Student {
         $result .= "\n";
         return '<pre>' . $result . '</pre>';
     }
-
 }
